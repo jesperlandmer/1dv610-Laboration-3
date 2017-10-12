08853a3d61a97ae2a295397adc6c8beba6702eb5
[Jesper] <[jp222vq@student.lnu.se]>
2017-10-11 21:36:26 -0700

    Edited comment to DateTimeView

--- view/DateTimeView.php
+++ view/DateTimeView.php
@@ -6,7 +6,8 @@ class DateTimeView {
 
 	public function showDateTimeFormat() : string
 	{
-		// Creates format "[Day], the [Date] of [Month] [Year], The time is [hh:mm:ss]"
+		// [Day], the [Date] of [Month] [Year], The time is [hh:mm:ss]
+		// Example: "Thursday, the 12th of October 2017, The time is 06:24:26"
 		$timeString = date('l, \t\h\e jS \of F Y, \T\h\e \t\i\m\e \i\s H:i:s');
 		
 		return '<p>' . $timeString . '</p>';
