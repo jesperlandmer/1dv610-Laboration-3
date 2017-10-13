fe3bded8cd2cefb909dd396a2f337538a115d669
[Jesper] <[jp222vq@student.lnu.se]>
2017-10-12 20:56:46 -0700

    Added database model

---
+++ model/DatabaseModel.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace model;
+
+require_once("dbHelpers/PDOService.php");
+
+class DatabaseModel
+{
+
+    private static $cost = "cost";
+
+    public function __construct()
+    {
+        $this->dbHelper = new PDOService();
+    }
+
+    public function getUserFromDatabase(string $username) : \PDOStatement
+    {
+        assert(isset($username));
+
+        return $this->dbHelper->findData(array(
+            PDOVariables::DB_USERNAME_COLUMN => $username
+        ));
+    }
+
+    public function updateUserFromDatabase(string $username, string $password) : \PDOStatement
+    {
+        assert(isset($username));
+        assert(isset($password));
+
+        return $this->dbHelper->editData(array(
+            PDOVariables::DB_USERNAME_COLUMN => $username,
+            PDOVariables::DB_PASSWORD_COLUMN => $this->getPasswordHash($password)
+        ));
+    }
+
+    public function saveUserToDatabase(string $username, string $password)
+    {
+        assert(isset($username));
+        assert(isset($password));
+
+        $this->userData = $this->dbHelper->saveData(array(
+            PDOVariables::DB_USERNAME_COLUMN => $username,
+            PDOVariables::DB_PASSWORD_COLUMN => $this->getPasswordHash($password)
+        ));
+    }
+
+    private function getPasswordHash(string $password) : string
+    {
+        return password_hash("$password", PASSWORD_BCRYPT, [self::$cost => 8]);
+    }
+
+    public function isExistingUser(string $username, strin $password) : bool
+    {
+        return password_verify($password, $this->getDBUserPassword($username));
+    }
+
+    public function getDBUserPassword(string $username) : string
+    {
+        return $this->persistentUser->getUserFromDatabase($username)->fetch()[PDOVariables::DB_PASSWORD_COLUMN];
+    }
+}
